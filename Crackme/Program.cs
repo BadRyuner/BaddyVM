3a31a021900437105c49cbd0fc38dce9fb513dbc
Andrey <54708336+BadRyuner@users.noreply.github.com>
2023-06-11 17:04:28 +0300

    init

---
+++ Crackme/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace Crackme;
+
+public unsafe class Program
+{
+	public Program(int a, int b, int c)
+	{
+		Console.WriteLine($"{a} - {b} - {c}");
+		Console.WriteLine(this.GetType());
+	}
+
+	public int aboba;
+	public long kek;
+
+	public Program()
+	{
+		aboba = 123;
+		kek = 456;
+	}
+
+	public static void Main(string[] args)
+	{
+		//var r = DoSomething();
+		//Console.WriteLine(r == null);
+		//Test(r);
+		//Test1(20, 40);
+		//foreach(var i in new int[] { 1,2,3,4,5,6,7,8,9,10 }) // no crash
+		//	Console.WriteLine(i);
+
+		//var list = new List<int>() { 1,2,3,4,5,6,7 };
+		//foreach(var i in list) // crash, thx Enumerator ValueType
+		//	Console.WriteLine(i);
+
+		var p = new Program();
+		Console.WriteLine($"{p.aboba}, {p.kek}");
+		p.aboba *= 2;
+		Console.WriteLine(p.aboba);
+		ref var k = ref AnotherTest(p);
+		k += 7; // bad
+		Console.WriteLine(k);
+	}
+
+	static ref long AnotherTest(Program p)
+	{
+		return ref p.kek;
+	}
+
+	static int ab = 0;
+
+	private static void Test1(int a, int b)
+	{
+		new Program(634, 32, 754);
+		Test2(634, 32, 754);
+		//Console.WriteLine($"a = {a}, b = {b}");
+	}
+
+	private static void Test2(int a, int b, int c)
+	{
+		Console.WriteLine($"{a} - {b} - {c}");
+	}
+
+	public static void Test(Program p)
+	{
+		//p.NonVirtKek();
+		p.Kek();
+	}
+
+	private static Program DoSomething()
+	{
+		Test1(77, 66);
+		var a = new Program(625, 982, 32);
+		return a;
+	}
+
+	public virtual void Kek()
+	{
+		Console.WriteLine("kek");
+	}
+
+	public virtual void Kek1()
+	{
+		Console.WriteLine("kek1");
+	}
+
+	public virtual void Kek2()
+	{
+		Console.WriteLine("kek2");
+	}
+
+	public virtual void Kek3()
+	{
+		Console.WriteLine("kek3");
+	}
+
+	public virtual void Kek4()
+	{
+		Console.WriteLine("kek4");
+	}
+
+	public virtual void Kek5()
+	{
+		Console.WriteLine("kek5");
+	}
+}
---
+++ README.md
@@ -0,0 +1,47 @@
+# BaddyVM
+Another poorly implemented virtual machine for C#.
+Good for crackme obfuscation to show how good you are.
+
+# What opcodes are implemented?
+[Click me](https://github.com/BadRyuner/BaddyVM/blob/master/BaddyVM/VM/VMCore.cs#L69)
+
+# How well does it work?
+Can add two times two and coolly call a virtual method that no one will know about it. (Using Washi theory) (It won't work with interfaces, coreclr will fall)
+It cant eat methods that returns ValueTypes larger than 8 bytes.
+Not all opcodes work correctly. 
+_*Made for fun.*_
+
+# How much slower will the code run?
+Slower than the Yandere-dev code
+
+# How to use?
+Download latest sources, open Program.cs, change it for yourself and compile it in Release mode.
+
+# How it looks
+```csharp
+public unsafe static void Main(string[] args)
+{
+	int* ptr = stackalloc int[64];
+	int* ptr2 = stackalloc int[120];
+	*(IntPtr*)(ptr2 + 0) = ptr;
+	*(IntPtr*)(ptr2 + 12) = args;
+	*(IntPtr*)(ptr2 + 10) = 1;
+	*(IntPtr*)(ptr2 + 4) = VMRunner.VMTable;
+	VMRunner.a(<Module>.Main(), ptr2);
+}
+```
+The VM instructions look like this
+```csharp
+internal unsafe static int* Store(int* A_0, int* A_1)
+{
+	int num = (int)(*(ushort*)A_0);
+	A_0 = (int*)((byte*)A_0 + 2);
+	ref IntPtr ptr = ref *(IntPtr*)(A_1 + num / 4);
+	int* ptr2 = *(long*)(A_1 + 0);
+	IntPtr intPtr = (IntPtr)(*(long*)ptr2);
+	ptr2 -= 2;
+	*(IntPtr*)(A_1 + 0) = ptr2;
+	ptr = intPtr;
+	jmp(Router());
+}
+```
